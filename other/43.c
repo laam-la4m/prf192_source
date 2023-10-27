ca4114666cfcc4064311b90226f5751e907c2e9b
Laam <lamnthe181560@fpt.edu.vn>
2023-10-27 14:00:45 +0700

    update 43.c

---
+++ other/43.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+    int i;
+    char a[20];
+    scanf ("%s", a);
+    size_t l = strlen(a);
+    if (l % 2 == 0)
+    {
+        return 0;
+    }
+  puts ("\nOUTPUT:\n");
+	  if (l > 5 && l % 2 == 0 && l < 20)
+    {
+        return 0;
+    }
+    else 
+    {
+    	for (i = (l/2 - 1) - 1; i <(l/2+1)+2; i++)
+        {
+            printf ("%c", a[i]);
+        }
+		}
+		
+
+
+
+
+
+
+  
+  
+  //--FIXED PART - DO NOT EDIT ANY THINGS HERE
+  printf("\n");
+  system ("pause");
+  return(0);
+}
