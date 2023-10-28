510477421e42abe343dd1ba2211f81280d04c218
Laam <lamnthe181560@fpt.edu.vn>
2023-10-28 14:05:40 +0700

    Update 44.c

---
+++ other/44.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a[7], count[100] = {0}, i, max = 0;
+
+    for (i = 0; i < 7; i++)
+    {
+        scanf("%d", &a[i]);
+        if (a[i] >= 10 && a[i] <= 99)
+        {
+            count[a[i]]++;
+            if (count[a[i]] > max)
+            {
+                max = count[a[i]];
+            }
+        }
+    }
+
+    puts("\nOUTPUT:");
+    if (max == 0)
+    {
+        printf ("no two-digits number");
+    }
+    else
+    {
+        for (i = 10; i < 100; i++)
+        {
+            if (count[i] == max)
+            {
+                printf("%d ", i);
+            }
+        }
+    }
+}
