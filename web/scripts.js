38278884d4e178f567b3aca02ffa30686afa08e8
Laam <lamnthe181560@fpt.edu.vn>
2023-11-01 10:51:41 +0700

    add some text

--- web/scripts.js
+++ web/scripts.js
@@ -1,12 +1,10 @@
 
 function showLibrary(libraryName) {
-    // Ẩn tất cả các thư viện
     var libraries = document.getElementsByClassName('library');
     for (var i = 0; i < libraries.length; i++) {
         libraries[i].style.display = "none";
     }
 
-    // Hiển thị thư viện được chọn
     document.getElementById(libraryName).style.display = "block";
 }
 document.getElementById("checkbox").addEventListener("change", function(event) {
