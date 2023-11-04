49682b489e132fd950db8afedad48d52aa4f26e7
Laam <lamnthe181560@fpt.edu.vn>
2023-11-04 20:07:20 +0700

    fix 71.c

--- [other]/71.c
+++ [other]/71.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
-int main ()
+
+int main()
 {
-    int n, i, j, deep = 999999999;
-    scanf ("%d", &n);
-    int a[n];
-    for (i = 0; i < n; i++)
+    //------------//
+    int n;
+    scanf("%d", &n);
+
+    int h[n];
+    for (int i = 0; i < n; i++)
     {
-        scanf ("%d", &a[i]);
+        scanf("%d", &h[i]);
     }
-    for (i = 0; i < n; i++)
+
+    int deep = -1;
+    for (int i = 1; i < n - 1; i++)
     {
-        if(a[i - 1] > a[i] && a[i] < a[i + 1] )
+        if (h[i - 1] > h[i] && h[i] < h[i + 1])
         {
-            if (a[i] < deep)
+            if (deep == -1 || h[i] < deep)
             {
-                deep = a[i];
+                deep = h[i];
             }
         }
     }
+    //------------//
     puts ("\nOUTPUT:");
-    printf ("%d", deep);
+    //------------//
+    printf("%d", deep);
 }
